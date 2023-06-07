a704061ef5c396dcdf9a8733347023849db8b380
SCBattisti <104858971+SCBattisti@users.noreply.github.com>
2023-06-07 11:43:11 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-  base: 'https://SCBattisti.github.io/socket-client-v2/',
+  base: 'https://scbattisti.github.io/socket-client-v2/',
   build: {
     outDir: '../dist'
   },
